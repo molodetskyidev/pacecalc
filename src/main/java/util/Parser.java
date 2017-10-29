59121b2364a2c54ace42090bfb4ad8b2f1c2f551
Oleksandr Molodetskyi <molodetskyidev@gmail.com>
2017-10-29 20:48:19 +0200

    added util package with Parser class and corresponded unit test, updated test suite

---
+++ src/main/java/util/Parser.java
@@ -0,0 +1,5 @@
+package util;
+// this class is util class to make parsing or other string manipulation
+public class Parser {
+
+}
---
+++ src/test/java/pacecalc/pacecalc/ParserTest.java
@@ -0,0 +1,9 @@
+package pacecalc.pacecalc;
+
+import org.testng.annotations.Test;
+
+public class ParserTest {
+  @Test
+  public void f() {
+  }
+}
