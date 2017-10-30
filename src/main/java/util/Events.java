f6b19234cd773806a67a171a80c489b57d5cfe7a
Oleksandr Molodetskyi <molodetskyidev@gmail.com>
2017-10-30 11:36:28 +0200

    added events list class

---
+++ src/main/java/util/Events.java
@@ -0,0 +1,15 @@
+package util;
+
+public class Events {
+	public int returnDistance(String evntName){
+		int evntDistance;
+		switch(evntName){
+		case "Marathon": evntDistance=42195; break;
+		case "Halfmarathon":evntDistance=21097;break;
+		case "10k":evntDistance=10000;break;
+		default:evntDistance=0; break;
+		}
+		return evntDistance;
+    
+	}
+}
---
+++ src/test/java/pacecalc/pacecalc/EventsTest.java
@@ -0,0 +1,25 @@
+package pacecalc.pacecalc;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.testng.annotations.Test;
+
+import junit.framework.Assert;
+import util.Events;
+
+public class EventsTest {
+  @Test
+  public void returnDistanceTest() {
+	  Events event = new Events();
+	  int eventDistance;
+	  eventDistance=event.returnDistance("Marathon");
+	  Assert.assertEquals("Test marathon distance return. Wrong value!", 42195, eventDistance);
+	  
+	  eventDistance=event.returnDistance("Halfmarathon");
+	  Assert.assertEquals("Test marathon distance return. Wrong value!", 21097, eventDistance);
+	  
+	  eventDistance=event.returnDistance("10k");
+	  Assert.assertEquals("Test marathon distance return. Wrong value!", 10000, eventDistance);
+  }
+}
