5f94af5a17fc2c941d1052ca9d7c496f81bd6b29
Oleksandr Molodetskyi <molodetskyidev@gmail.com>
2017-10-30 11:37:01 +0200

    changed main program method to show Entry message

--- src/main/java/pacecalc/pacecalc/App.java
+++ src/main/java/pacecalc/pacecalc/App.java
@@ -1,15 +1,21 @@
 package pacecalc.pacecalc;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 
 {
     public static void main( String[] args )
     {
-    	String msg ="Hello World!";
-        System.out.print(msg);
+    	String lineseparator=System.getProperty("line.separator");
+    	String entryMsg ="************* Welcome to Pace Calculator! *************"+lineseparator+lineseparator+"Please select what you would like to do:"+lineseparator +
+    			"* If you would like to calculate pace based on time and distance enter 1;"+lineseparator +
+    			"* If you would like to time based on pace and distance enter 2;"+lineseparator +
+    			"* If you would like to distance based on time and pace enter 3;"+lineseparator +
+    			"* If you would like to exit enter 0;"+lineseparator +lineseparator +
+    			"Enter your choice here:";
+    	
+    	
+        System.out.println(entryMsg);
+        
     }
 }
