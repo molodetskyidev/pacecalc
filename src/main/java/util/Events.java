852dfa0df3ae0e102eaaeaf459696f530aa4dcd1
molodetskyidev <33019295+molodetskyidev@users.noreply.github.com>
2017-11-04 16:37:06 +0200

    Update Events.java
    commented code which failed the build

--- src/main/java/util/Events.java
+++ src/main/java/util/Events.java
@@ -2,13 +2,15 @@ package util;
 
 public class Events {
 	public int returnDistance(String evntName){
-		int evntDistance;
+		int evntDistance=0;
+		/*
 		switch(evntName){
 		case "Marathon": evntDistance=42195; break;
 		case "Halfmarathon":evntDistance=21097;break;
 		case "10k":evntDistance=10000;break;
 		default:evntDistance=0; break;
 		}
+		*/
 		return evntDistance;
     
 	}
