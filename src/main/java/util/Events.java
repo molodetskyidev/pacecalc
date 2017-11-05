0c09e95064132d8fbdfd13c96860a285b9786da4
Oleksandr Molodetskyi <molodetskyidev@gmail.com>
2017-11-05 15:05:49 +0200

    changed events from switch/case to HashMap

--- src/main/java/util/Events.java
+++ src/main/java/util/Events.java
@@ -1,28 +1,23 @@
 package util;
 
+import java.util.HashMap;
+
 public class Events {
 
 	// method returns distance depends on event
 
-	public int returnDistance(String evntName) {
-		int evntDistance = 0;
-
-		switch (evntName) {
-		case "Marathon":
-			evntDistance = 42195;
-			break; // marathon event
-		case "Halfmarathon":
-			evntDistance = 21097;
-			break; // halfmarathon event
-		case "10k":
-			evntDistance = 10000;
-			break; // 10 000 m event
-		default:
-			evntDistance = 0;
-			break; // if nothing return 0
-		}
-
-		return evntDistance;
+	public int returnDistance(String eventName) {
+		try{
+			 HashMap<String, Integer> eventArray=new HashMap();
+			 eventArray.put("Marathon", 42195);
+			 eventArray.put("Halfmarathon", 21097);
+			 eventArray.put("10k", 10000);
+				return eventArray.get(eventName);
+				
+		 } catch(NullPointerException e){
+			 return 0; 
+		 }
+		
 
 	}
 }
