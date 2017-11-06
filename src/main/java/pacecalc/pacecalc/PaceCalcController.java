1f5df34c8846b5ce98e8b6391f6984d1fe6e7b9e
Oleksandr Molodetskyi <molodetskyidev@gmail.com>
2017-11-06 12:31:38 +0200

    fixed gitignore and fixed yes/no issue in controller

--- src/main/java/pacecalc/pacecalc/PaceCalcController.java
+++ src/main/java/pacecalc/pacecalc/PaceCalcController.java
@@ -15,165 +15,164 @@ public class PaceCalcController {
 		String distanceStr;
 		String paceStr;
 		String answer;
-		
+
 		cli.showWelcomeMsg();
 		cli.showSelectionMsg();
 		Scanner userInput = new Scanner(System.in);
 		// PaceCalc pacecalc=new PaceCalc();
 		Parser parser = new Parser();
-		try{
-		int userSelection = userInput.nextInt();
-		// TODO looks like I don't need setters in this function should think
-		// about their deletion
-		switch (userSelection) {
-		case 1:
-			cli.showCalculatePaceMsg();
-			cli.showEnterTimeMsg();
-			timeStr = userInput.next();
-			timeInt = parser.timeStringToInt(timeStr);
-			while(timeInt<=0){
-				cli.showErrorMsg();
+		try {
+			int userSelection = userInput.nextInt();
+			// TODO looks like I don't need setters in this function should
+			// think
+			// about their deletion
+			switch (userSelection) {
+			case 1:
+				cli.showCalculatePaceMsg();
 				cli.showEnterTimeMsg();
 				timeStr = userInput.next();
 				timeInt = parser.timeStringToInt(timeStr);
-			}
-			// pacecalc.setTime(timeInt);
-			cli.showEnterDistanceMsg();
-			distanceStr = userInput.next();
-			distanceInt = parser.distanceStringToInt(distanceStr);
-			while(distanceInt<0){
-				cli.showErrorMsg();
+				while (timeInt <= 0) {
+					cli.showErrorMsg();
+					cli.showEnterTimeMsg();
+					timeStr = userInput.next();
+					timeInt = parser.timeStringToInt(timeStr);
+				}
+				// pacecalc.setTime(timeInt);
 				cli.showEnterDistanceMsg();
 				distanceStr = userInput.next();
 				distanceInt = parser.distanceStringToInt(distanceStr);
-			}
-			// pacecalc.setTime(distanceInt);
-			paceInt = pacecalc.calcPace(distanceInt, timeInt);
-			paceStr = parser.parseFromPace(paceInt);
-			cli.showCalculatedPace(paceStr);
-			cli.showContinueMsg();
-			answer = userInput.next();
-			while(!answer.equals("yes")&&!answer.equals("no")){ 
-				cli.showErrorMsg();
+				while (distanceInt < 0) {
+					cli.showErrorMsg();
+					cli.showEnterDistanceMsg();
+					distanceStr = userInput.next();
+					distanceInt = parser.distanceStringToInt(distanceStr);
+				}
+				// pacecalc.setTime(distanceInt);
+				paceInt = pacecalc.calcPace(distanceInt, timeInt);
+				paceStr = parser.parseFromPace(paceInt);
+				cli.showCalculatedPace(paceStr);
 				cli.showContinueMsg();
 				answer = userInput.next();
-			}
-			if (answer.equals("yes")) {
-				CalcFlow(pacecalc, cli);
-			} else if (answer.equals("no")) {
-				cli.showExitMsg();
-				System.exit(0);
-			} 
-			break;
-		case 2:
-			cli.showCalculateTimeMsg();
-			cli.showEnterDistanceMsg();
-			distanceStr = userInput.next();
-			distanceInt = parser.distanceStringToInt(distanceStr);
-			while(distanceInt<0){
-				cli.showErrorMsg();
+				while (!answer.equals("yes") && !answer.equals("no")) {
+					cli.showErrorMsg();
+					cli.showContinueMsg();
+					answer = userInput.next();
+				}
+				if (answer.equals("yes")) {
+					CalcFlow(pacecalc, cli);
+				} else if (answer.equals("no")) {
+					cli.showExitMsg();
+					System.exit(0);
+				}
+				break;
+			case 2:
+				cli.showCalculateTimeMsg();
 				cli.showEnterDistanceMsg();
 				distanceStr = userInput.next();
 				distanceInt = parser.distanceStringToInt(distanceStr);
-			}
-			
-			// pacecalc.setDistance(distanceInt);
-			cli.showEnterPaceMsg();
-			paceStr = userInput.next();
-			paceInt = parser.paceStringToInt(paceStr);
-			while(paceInt<0){
-				cli.showErrorMsg();
+				while (distanceInt < 0) {
+					cli.showErrorMsg();
+					cli.showEnterDistanceMsg();
+					distanceStr = userInput.next();
+					distanceInt = parser.distanceStringToInt(distanceStr);
+				}
+
+				// pacecalc.setDistance(distanceInt);
 				cli.showEnterPaceMsg();
 				paceStr = userInput.next();
 				paceInt = parser.paceStringToInt(paceStr);
-			}
-			// pacecalc.setPace(paceInt);
-			timeInt = pacecalc.calcTime(paceInt, distanceInt);
-			timeStr = parser.parseFromTime(timeInt);
-			cli.showCalculatedTime(timeStr);
-			cli.showContinueMsg();
-			answer = userInput.next();
-			while(!answer.equals("yes")&&!answer.equals("no")){
-				cli.showErrorMsg();
+				while (paceInt < 0) {
+					cli.showErrorMsg();
+					cli.showEnterPaceMsg();
+					paceStr = userInput.next();
+					paceInt = parser.paceStringToInt(paceStr);
+				}
+				// pacecalc.setPace(paceInt);
+				timeInt = pacecalc.calcTime(paceInt, distanceInt);
+				timeStr = parser.parseFromTime(timeInt);
+				cli.showCalculatedTime(timeStr);
 				cli.showContinueMsg();
 				answer = userInput.next();
-			}
-			if (answer.equals("yes")) {
-				CalcFlow(pacecalc, cli);
-			} else if (answer.equals("no")) {
-				cli.showExitMsg();
-				System.exit(0);
-			}
-			
-			break;
-		case 3:
-			cli.showCalculateDistanceMsg();
-			cli.showEnterPaceMsg();
-			paceStr = userInput.next();
-			paceInt = parser.paceStringToInt(paceStr);
-			while(paceInt<0){
-				cli.showErrorMsg();
+				while (!answer.equals("yes") && !answer.equals("no")) {
+					cli.showErrorMsg();
+					cli.showContinueMsg();
+					answer = userInput.next();
+				}
+				if (answer.equals("yes")) {
+					CalcFlow(pacecalc, cli);
+				} else if (answer.equals("no")) {
+					cli.showExitMsg();
+					System.exit(0);
+				}
+
+				break;
+			case 3:
+				cli.showCalculateDistanceMsg();
 				cli.showEnterPaceMsg();
 				paceStr = userInput.next();
 				paceInt = parser.paceStringToInt(paceStr);
-			}
-			
-			// pacecalc.setPace(paceInt);
-			cli.showEnterTimeMsg();
-			timeStr = userInput.next();
-			timeInt = parser.timeStringToInt(timeStr);
-			while(timeInt<0){
-				cli.showErrorMsg();
+				while (paceInt < 0) {
+					cli.showErrorMsg();
+					cli.showEnterPaceMsg();
+					paceStr = userInput.next();
+					paceInt = parser.paceStringToInt(paceStr);
+				}
+
+				// pacecalc.setPace(paceInt);
 				cli.showEnterTimeMsg();
 				timeStr = userInput.next();
 				timeInt = parser.timeStringToInt(timeStr);
-			}
-			// pacecalc.setTime(timeInt);
-			distanceInt = pacecalc.calcDistance(paceInt, timeInt);
-			distanceStr = parser.parseFromDistance(distanceInt);
-			cli.showCalculatedDistance(distanceStr);
-			cli.showContinueMsg();
-			answer = userInput.next();
-			while(!answer.equals("yes")&&!answer.equals("no")){
-				cli.showErrorMsg();
+				while (timeInt < 0) {
+					cli.showErrorMsg();
+					cli.showEnterTimeMsg();
+					timeStr = userInput.next();
+					timeInt = parser.timeStringToInt(timeStr);
+				}
+				// pacecalc.setTime(timeInt);
+				distanceInt = pacecalc.calcDistance(paceInt, timeInt);
+				distanceStr = parser.parseFromDistance(distanceInt);
+				cli.showCalculatedDistance(distanceStr);
 				cli.showContinueMsg();
 				answer = userInput.next();
-			}
-			if (answer.equals("yes")) {
-				CalcFlow(pacecalc, cli);
-			} else if (answer.equals("no")) {
-				cli.showExitMsg();
-				System.exit(0);
-			}
-			
-			break;
-		case 0:
-			cli.showExitQuestionMsg();
-			answer = userInput.next();
-			
-			while(!answer.equals("yes")&&!answer.equals("no")){
-				cli.showErrorMsg();
+				while (!answer.equals("yes") && !answer.equals("no")) {
+					cli.showErrorMsg();
+					cli.showContinueMsg();
+					answer = userInput.next();
+				}
+				if (answer.equals("yes")) {
+					CalcFlow(pacecalc, cli);
+				} else if (answer.equals("no")) {
+					cli.showExitMsg();
+					System.exit(0);
+				}
+
+				break;
+			case 0:
 				cli.showExitQuestionMsg();
 				answer = userInput.next();
-			}
-			if (answer.equals("no")) {
+
+				while (!answer.equals("yes") && !answer.equals("no")) {
+					cli.showErrorMsg();
+					cli.showExitQuestionMsg();
+					answer = userInput.next();
+				}
+				if (answer.equals("no")) {
+					CalcFlow(pacecalc, cli);
+				} else if (answer.equals("yes")) {
+					cli.showExitMsg();
+					System.exit(0);
+				}
+				break;
+			default:
+				cli.showErrorMsg();
 				CalcFlow(pacecalc, cli);
-			} else if (answer.equals("yes")) {
-				cli.showExitMsg();
-				System.exit(0);
+				break;
 			}
-			break;
-		default:
+		} catch (NumberFormatException e) {
 			cli.showErrorMsg();
 			CalcFlow(pacecalc, cli);
-			break;
-		}
-		}
-		catch(NumberFormatException e){
-			cli.showErrorMsg();
-			CalcFlow(pacecalc, cli);
-		}
-		catch(InputMismatchException e){
+		} catch (InputMismatchException e) {
 			cli.showErrorMsg();
 			CalcFlow(pacecalc, cli);
 		}
