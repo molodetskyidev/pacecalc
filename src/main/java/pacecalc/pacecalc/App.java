dedeeacc0b852923055d8a9d8b4365b89300fdfc
Oleksandr Molodetskyi <molodetskyidev@gmail.com>
2017-11-06 21:32:00 +0200

    test push into master

--- src/main/java/pacecalc/pacecalc/App.java
+++ src/main/java/pacecalc/pacecalc/App.java
@@ -3,6 +3,7 @@ package pacecalc.pacecalc;
 public class App
 
 {
+	//test push into master
 	public static void main(String[] args) {
 		PaceCalcController pcController = new PaceCalcController();
 		CLIuserInterface cli = new CLIuserInterface();
