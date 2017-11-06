51957405756973166b5092db32364696fccb9b6a
Oleksandr Molodetskyi <molodetskyidev@gmail.com>
2017-11-06 21:26:02 +0200

    first commit for version2, added -cli argument

--- src/main/java/pacecalc/pacecalc/App.java
+++ src/main/java/pacecalc/pacecalc/App.java
@@ -4,10 +4,24 @@ public class App
 
 {
 	public static void main(String[] args) {
-		PaceCalcController pcController = new PaceCalcController();
-		CLIuserInterface cli = new CLIuserInterface();
-		PaceCalc pacecalc = new PaceCalc();
-		pcController.CalcFlow(pacecalc,cli);
-
+		
+		try{
+			if(args[0].equals("-cli")){
+			PaceCalcController pcController = new PaceCalcController();
+			CLIuserInterface cli = new CLIuserInterface();
+			PaceCalc pacecalc = new PaceCalc();
+			pcController.CalcFlow(pacecalc,cli);
+			}
+		
+		else{
+			System.out.println("you entered "+args[0]);
+			System.out.println("UI is not implemented yet");
+		}
+		
+		 }
+		catch(ArrayIndexOutOfBoundsException e){
+			System.out.println("UI is not implemented yet. In order to use application in command line, please run application with paramenter -cli");
+		}
+		
 	}
 }
